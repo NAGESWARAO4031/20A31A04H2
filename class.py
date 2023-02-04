9c0b77e0a7251025cb05831d099e05daa244bb00
Magapu Datta Siva Nageswarao <113781954+NAGESWARAO4031@users.noreply.github.com>
2023-02-04 15:04:36 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,10 @@
+# program tocreate and access an obj
+class xyz:
+    var=100
+class abc:
+    attribute1=10
+    def display(self):
+        print("this is in class")
+obj=abc()
+print(obj.attribute1)
+obj.display()
